9197b5318c82c96779862ef144005e6e56051785
saniaakhann <142003312+saniaakhann@users.noreply.github.com>
2026-01-13 13:41:36 +0000

    datatype

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,12 @@
+"use strict" // treat code as newer version altough automatic
+//alert(3+3) using node js not browser diff syntax
+let name="string"
+let age=18
+// number 2 to power 53
+//big int more power
+let islogged=false
+let state
+console.log(typeof "sania")
+console.log(typeof age)
+console.log(typeof null)//interview q object 
+console.log(typeof undefined)//undefined
--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,14 @@
 const accountId= 14453
 let accountEmail="hitsh@google.com"
 var accountPassword="12345"
-accountCity="jaipur"
+accountCity="jaipur"//ho skta hai
+let accountState
 
-accountId=2;
-console.log(accountId)
+//accountId=2; not allowed
+//console.log(accountId)
+accountEmail="sania@googlr.com"
+accountPassword="ddwssf"
+accountCity="delhi"
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+/* prefer not to use var block scope issue*/
