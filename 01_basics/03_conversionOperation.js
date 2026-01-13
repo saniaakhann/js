2dc16f2e04e7762847d8ac116dfb15f81c4dea92
saniaakhann <142003312+saniaakhann@users.noreply.github.com>
2026-01-13 13:55:21 +0000

    CONVERSION OF DATATYPE

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score="33abc"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber= Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+// 33=33
+//33abc=NaN
+//TRUE=1
+//NULL = 0
+    //AFTER BOOL CONVERSION
+//""=FALSE
+//"SANIA"=TRUE
