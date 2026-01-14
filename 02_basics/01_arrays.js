8d5d401de5b1d01055702a84c8490da6096d2563
saniaakhann <142003312+saniaakhann@users.noreply.github.com>
2026-01-14 21:41:05 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,25 @@
+// arrays
+const myArr=[0,1,2,3,4,5,6]
+const myHeros=["srk","aaryan"]
+const myArr2=new Array(1,2,3,4,)
+console.log(myArr[0])
+//INT QUES It makes shallow copy same ref point not deep one
+
+//array methods
+myArr.push(6)//add in end
+myArr.pop()//remove last value
+myArr.unshift(9) // starting m add krta h unoptimised
+myArr.shift()//starting remove
+console.log(myArr)
+
+
+console.log(myArr.includes(9))//datatype bool
+
+//imp 
+const newArr=myArr.join()//string m covert krdeta h 
+
+//slice ,splice interview q
+console.log("A",myArr
+
+
+
